ddab4dcfbb55f880b8849095277a61c66826dc81
KlimenkoAV <KlimenkoAV@sl-grp.ru>
2023-12-07 08:09:24 +0400

    07.12.23 08:10

--- src/org/example/homework/Homework1.java
+++ src/org/example/homework/Homework1.java
@@ -1,11 +1,15 @@
 package org.example.homework;
 
+import static org.example.homework.Product.*;
+import static org.example.homework.TaxRates.*;
+
 public class Homework1 {
     public static void main(String[] args) {
         ex1();
         ex2();
         ex3();
         ex4();
+        ex4_1();
     }
 
     public static void ex1() {
@@ -184,5 +188,121 @@ public class Homework1 {
 
         System.out.println(profit);
 
+    }
+
+    public static void ex4_1() {
+        //Компания Рога и Копыта производит мясные продукты.
+        //Перечень производимых товаров :
+        //Колбаса - стоимость 800 руб,
+        //себестоимость при производстве меньше 1000 кг - 412руб,
+        //себестоимость при производстве от 1000 до 2000 (не включая) - 408 руб
+        //себестоимость при производстве от 2000кг - 404 руб
+
+        //Ветчина - стоимость 350 руб
+        //себестоимость при производстве - 275 руб
+
+        //Шейка - стоимость 500 руб
+        //себестоимость при производстве меньше 500кг - 311 руб
+        //себестоимость при производстве больше или равно 500кг - 299 руб
+
+        //Финансовые показатели
+        //Доход компании считается как умножение стоимости на количество проданных кг
+        //Расход компании считается как умножение себестоимости на количество проданных кг + миллион рублей
+        //Прибыль до налогов считается как: доход - расход
+        //Налоги считаются так:
+        // прибыль до налогов больше 2_000_000, облагается ставкой 13%
+        // прибыль до налогов больше 1_000_000 от 2_000_000, облагается ставкой 10%
+        // прибыль до налогов меньше 1_000_000, облагается ставкой 8%
+        //пример расчета налогов для прибыли до налогов 2_500_000:
+        //1_000_000 - налог 80_000 - по ставке 8%
+        //1_000_000 - налог 100_000 - по ставке 10%
+        //500_000 - 65_000 - по ставке 13%
+        //Итоговый налог: 80_000 + 100_000 + 65_000 = 245_000
+        //Прибыль после налогов: прибыль до налогов - налог.
+
+        //Необходимо создать универсальную систему расчетов прибыли после налогов,
+        //Т.е на вход подаются данные по количеству произведенных продуктов
+        // и печатается прибыль после налогов компании
+        //Узнать прибыль после налогов, при продаже:
+        //Колбасы 2000кг
+        //Ветчины 8511кг
+        //Шейки 6988кг
+
+        int quantitySausage = 2000;
+        int productionCostSausage = getProductCost(SAUSAGE, quantitySausage);
+
+        int quantityHam = 8511;
+        int productionCostHam = getProductCost(HAM, quantityHam);
+
+        int quantityNeck = 6988;
+        int productionCostNeck = getProductCost(NECK, quantityNeck);
+
+        long overheads = 1_000_000;
+
+        long incomeAll = calcIncome(
+                new int[]{SAUSAGE.getSellingPrice(), HAM.getSellingPrice(), NECK.getSellingPrice()},
+                new int[]{quantitySausage, quantityHam, quantityNeck});
+
+        long expensesAll = calcExpenses(
+                new int[]{productionCostSausage, productionCostHam, productionCostNeck},
+                new int[]{quantitySausage, quantityHam, quantityNeck}, overheads);
+
+        long marginalProfit = incomeAll - expensesAll;
+
+        //Расчет налогов
+        long calculationBase = marginalProfit;
+        double taxTotal = calcTax(calculationBase, marginalProfit);
+        double profit = marginalProfit - taxTotal;
+
+        System.out.println(profit);
+    }
+
+    private static long calcIncome(int[] prices, int[] quality) {
+        long result = 0;
+        for (int i = 0; i < prices.length; i++) {
+            result += prices[i] * quality[i];
+        }
+        return result;
+    }
+
+    private static long calcExpenses(int[] cost, int[] quality, long overheads) {
+        return calcIncome(cost, quality) + overheads;
+    }
+
+    private static double calcTax(long calculationBase, long marginalProfit) {
+        double taxTotal = 0;
+        if (marginalProfit <= TAX_BEFORE_ONE_MILLION.getLimit()) {
+            taxTotal = TAX_BEFORE_ONE_MILLION.getTaxRate() * calculationBase;
+        } else if ((marginalProfit > TAX_BEFORE_ONE_MILLION.getLimit()) && (marginalProfit <= TAX_BEFORE_TWO_MILLION.getLimit())) {
+            taxTotal = TAX_BEFORE_ONE_MILLION.getTaxRate() * 1_000_000;
+            calculationBase = calculationBase - 1_000_000;
+            taxTotal += TAX_BEFORE_TWO_MILLION.getTaxRate() * calculationBase;
+        } else if (marginalProfit > TAX_BEFORE_ONE_MILLION.getLimit() && marginalProfit < TAX_AFTER_TWO_MILLION.getLimit()) {
+            taxTotal = TAX_BEFORE_ONE_MILLION.getTaxRate() * 1_000_000;
+            calculationBase = calculationBase - 1_000_000;
+            taxTotal += TAX_BEFORE_TWO_MILLION.getTaxRate() * 1_000_000;
+            calculationBase = calculationBase - 1_000_000;
+            taxTotal += TAX_AFTER_TWO_MILLION.getTaxRate() * calculationBase;
+        }
+        return taxTotal;
+    }
+
+    private static int getProductCost(Product product, int quantity) {
+        if (product == SAUSAGE) {
+            if (quantity < 1000) {
+                return 412;
+            } else if (quantity >= 1000 && quantity < 2000) {
+                return 408;
+            }
+            return 404;
+        } else if (product == Product.HAM) {
+            return 275;
+        } else if (product == NECK) {
+            if (quantity < 500) {
+                return 311;
+            }
+            return 299;
+        }
+        throw new RuntimeException("Неизвестный тип продукта.");
     }
 }
