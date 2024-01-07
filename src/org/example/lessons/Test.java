908e2b21e2fedda0b5eed97d67fda4f88d46afdb
KlimenkoAV <KlimenkoAV@sl-grp.ru>
2024-01-07 18:21:35 +0400

    07.01.24

---
+++ src/org/example/lessons/Test.java
@@ -0,0 +1,19 @@
+package org.example.lessons;
+
+public class Test {
+
+    String name;
+
+    public Test(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return converter(name);
+    }
+
+    private static String converter(String name) {
+        return name.trim().toUpperCase();
+    }
+
+}
