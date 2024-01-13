d0102f9f5de258a283804c95475392e93068fe9b
KlimenkoAV <KlimenkoAV@sl-grp.ru>
2024-01-13 20:37:07 +0400

    13.01.24

---
+++ src/org/javaacademy/module2/homework/Homework1/ex2/Runner.java
@@ -0,0 +1,12 @@
+package org.javaacademy.module2.homework.Homework1.ex2;
+
+/*Создать два List с числами
+Первый List: 5,2,4
+Второй List: 7,10
+Создать финальный лист и заполнить его данными из первого и второго листа, не используя for each.
+Удалить 3 элемент в финальном листе. Вывести на экран индекс числа 10.*/
+public class Runner {
+    public static void main(String[] args) {
+
+    }
+}
