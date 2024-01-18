e7dc0b1cc04281da338cbd921f6cb0ae84ec7cc7
KlimenkoAV <KlimenkoAV@volgir.com>
2024-01-18 13:08:13 +0400

    18.01.24

--- src/org/javaacademy/module2/lessons/lesson2/ex1/Runner.java
+++ src/org/javaacademy/module2/lessons/lesson2/ex1/Runner.java
@@ -1,13 +1,15 @@
 package org.javaacademy.module2.lessons.lesson2.ex1;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 public class Runner {
     public static void main(String[] args) {
         Integer[] ints = {6, 6, 2, 3, 4, 5};
         List<Integer> imList = List.of(ints);
-        ArrayList<Integer> integers = new ArrayList<Integer>(imList);
+        ArrayList<Integer> integers = new ArrayList<>(imList);
         //Добавляем элементы
 //        integers.add(1);
 //        integers.add(2);
@@ -15,6 +17,9 @@ public class Runner {
 //        integers.add(4);
 //        integers.add(5);
 
+        System.out.println("Вывод коллекции:" + integers);
+        System.out.println("Соодержится ли 2 и 6:" + integers.containsAll(List.of(2, 6)));
+        System.out.println("Удаление 2 и 4:" + integers.removeAll(List.of(4, 2)));
         System.out.println("Вывод коллекции:" + integers);
         System.out.printf("Содержит 10: %s\n", integers.contains(10));
         integers.remove(new Integer(6));
@@ -27,11 +32,21 @@ public class Runner {
         strings.add(2, "E");
         System.out.println(strings);
         strings.addAll(0, List.of("X", "Y", "Z"));
+
         System.out.println("Вывод коллекции после добавления другой коллекции:" + strings);
+
+        // перенос в массив
+        System.out.println("перенос в массив");
+        String[] array = strings.toArray(new String[strings.size() - 1]);
+        System.out.println(Arrays.toString(array));
+
         System.out.println("Индекс Х: " + strings.indexOf("X"));
+        System.out.println("Индекс Q: " + strings.indexOf("Q"));
 
         strings.clear();
         System.out.println("Вывод после очистки:" + strings);
+        System.out.println(Arrays.toString(array));
+
         //Неизменяемый лист (не можем удалять, добавлять и изменять элементы)
         List<String> immutableList = List.of("D", "C");
         //будет ошибка
@@ -39,7 +54,8 @@ public class Runner {
 
 
         //["A", "B", "C", "D", null, null ...]
-        //[null, null , null , null , "A", "B", "C", "D"]
+        //[null, null , null , "A", "B", "C", "D"]
+
 
     }
 }
